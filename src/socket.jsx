d6e28eb172adbd3049b2925aa5f71eb160685115
Ben Shuan <benshuan@gmail.com>
2024-09-21 17:26:30 +0300

    .

--- src/socket.jsx
+++ src/socket.jsx
@@ -2,7 +2,6 @@ import { io } from 'socket.io-client';
 
 export const socket = io("wss://immense-bayou-69622-27b3bda0f510.herokuapp.com/",{
   transports: ['websocket', 'polling', 'flashsocket'],
-  path: '/SERVERPATH',
   forceNew: true,
   reconnectionAttempts: 3,
   timeout: 2000,
