59908f4f7159086a4e3c0e83a48482c1bb0a6f55
Ben Shuan <benshuan@gmail.com>
2024-09-21 17:24:36 +0300

    update socket

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.VITE_APP_SERVER_URL;
-
-export const socket = io(URL);
+export const socket = io("wss://immense-bayou-69622-27b3bda0f510.herokuapp.com/",{
+  transports: ['websocket', 'polling', 'flashsocket'],
+  path: '/SERVERPATH',
+  forceNew: true,
+  reconnectionAttempts: 3,
+  timeout: 2000,
+});
