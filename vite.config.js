45341547585152a07d75d38f12429fa05942a787
Ben Shuan <benshuan@gmail.com>
2024-09-21 15:33:34 +0300

    initial-commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+
 })
+git init
+git add .
+git commit -m "initial-commit"
+git branch -M main
+git remote add origin https://github.com/BenShuan/jamoveo-client.git
+git push -u origin main
